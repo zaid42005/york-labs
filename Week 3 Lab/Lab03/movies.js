8346a645fd07c8467adaf2288d715e63ea76b639
zaid42005 <zaid.42005@gmail.com>
2024-02-22 19:01:38 -0500

    ex1 of lab 3 done moving to ex2

---
+++ Week 3 Lab/Lab03/movies.js
@@ -0,0 +1,78 @@
+function p01Func() {
+	
+  /* in Ex2, add a getElementById here to get the "description" div 
+     and set its innerHTML to <p>...</p>
+  */
+
+
+  /* in Ex2, add a getElementById here to get "poster" img and set
+     its attribue src to solaris_poster.jpg. Note that movie images are 
+	  in the subfolder solarie and inside the images folder.
+  */
+
+  
+  /* in Ex2, add a getElementById here to get the "poster" img 
+     and set the display property of its style to none.
+  */
+
+  
+  /* in Ex2, repeat the above two steps for the "director" and "actors"
+     img elements here. For the image that is shown for "director", 
+     use solaris_director.jpg. For the image that is shown for "actors",
+     use solaris_actors.jpg
+  */
+ 
+ 
+  /* the following two lines retrieve the checkboxes with IDs check1
+     check2 and check3, and unchecks them.
+  */
+
+  document.getElementById("check1").checked=false;
+  document.getElementById("check2").checked=false;
+  document.getElementById("check3").checked=false;
+
+}
+
+
+/* in Ex3, uncomment the following functions & complete them */
+
+/* remove this line
+
+function checkUncheck1(){
+  if (document.getElementById("check1").checked==true) {
+     // add a call to getElementById here to get the "poster" img and 
+     // set the display property of its style to "inline"	 	 
+  }
+  else {
+     // add a getElementById here to get "poster" img and 
+     // set the display property of its style to "none"	 
+  }
+}
+
+function checkUncheck2(){
+  if (document.getElementById("check2").checked==true) {
+     // add a getElementById here to get "director" img and 
+     // set the display property of its style to "inline"	 
+  }
+  else {
+     // add a getElementById here to get "director" img and 
+     // set the display property of its style to "none"	   
+  }
+}
+
+function checkUncheck3(){
+  if (document.getElementById("check3").checked==true) {
+     // add a getElementById here to get "actors" img and 
+     // set the display property of its style to "inline"	 
+  }
+  else {
+     // add a getElementById here to get "actors" img and 
+     // set the display property of its style to "none"	   
+  }
+}
+
+remove this line */
+
+/* in Ex4, create function p02Func() that is similar to p01Func() */
+
+/* in Ex5, create the functions zoomIn() and zoomOut() */
